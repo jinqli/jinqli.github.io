595d2429991e83e12dde0941386854296b015212
孑然一身 <ljqrongyao@outlook.com>
2021-11-26 17:12:26 +0800

    inital

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: "mini-ui",
+  description: "描述",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/logo.png",
+      },
+    ],
+  ],
+  themeConfig: {
+    lastUpdated: "最后更新时间",
+    sidebar: {
+      "pages/mini-ui/": ["layout", "button", "menu"],
+    },
+    navbar: [
+      // NavbarItem
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "组件",
+        children: [
+          {
+            text: "uniapp-vue2",
+            link: "/pages/component.md",
+          },
+        ],
+      },
+      // NavbarGroup
+      {
+        text: "文档",
+        children: [
+          {
+            text: "mini-ui",
+            link: "/pages/mini-ui/mini-ui.md",
+          },
+        ],
+      },
+      {
+        text: "关于",
+        link: "/pages/about.md",
+      },
+      // 字符串 - 页面文件路径
+      // "/bar/README.md",
+    ],
+  },
+};
---
+++ docs/pages/mini-ui/layout.md
@@ -0,0 +1,43 @@
+# 布局样式
+
+
+
+## grid
+
+
+
+```html
+<view class='m-grid'>
+	<view class="m-grid-item"></view>
+    <view class="m-grid-item"></view>
+    <view class="m-grid-item"></view>
+    <view class="m-grid-item"></view>
+</view>
+```
+
+
+
+## flex-column
+
+```html
+<view class='m-flex-c'>
+	<view class="m-flex-item"></view>
+    <view class="m-flex-item"></view>
+    <view class="m-flex-item"></view>
+    <view class="m-flex-item"></view>
+</view>
+```
+
+
+
+## flex-row
+
+```html
+<view class='m-flex-r'>
+	<view class="m-flex-item"></view>
+    <view class="m-flex-item"></view>
+    <view class="m-flex-item"></view>
+    <view class="m-flex-item"></view>
+</view>
+```
+

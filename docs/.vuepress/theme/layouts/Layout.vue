8ffd127c55e78093a6b2035dc2842d15cb54b723
孑然一身 <ljqrongyao@outlook.com>
2021-11-27 10:58:08 +0800

    update .md

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,13 @@
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<template>
+  <div class="theme-container">
+    <Content />
+  </div>
+</template>
